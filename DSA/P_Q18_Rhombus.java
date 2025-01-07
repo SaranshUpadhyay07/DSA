15174d782dde4c57e2ddca0c42daf01e8edb1f64
SaranshUpadhyay07 <upadhyay.saransh17@gmail.com>
2025-01-07 18:25:47 +0530

    P_Q18_Rhombus.java modified

--- DSA/P_Q18_Rhombus.java
+++ DSA/P_Q18_Rhombus.java
@@ -5,35 +5,15 @@ import java.util.Scanner;
 public class P_Q18_Rhombus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the length of the heart:");
-        int heart = sc.nextInt();
-        for(int i = 1; i <= heart/2; i++){
-            for(int j =heart/2; j >i; j--){
-                System.out.print("  ");
-            }
-            for(int j =1; j <=i; j++){
-                System.out.print("*   ");
-
-            }
-            for(int j =1;j<=(heart/2-i)*2; j++){
-                System.out.print("  ");
-            }
-            for(int j =1; j <=i; j++){
-                System.out.print("*   ");
-            }
-            System.out.println();
-
-        }
-        for(int i =1; i<=heart; i++ ){
+        System.out.print("Enter the length of the side of the rhombus:");
+        int side = sc.nextInt();
+        for(int i =1; i<=side;i++){
             for(int j =1;j<i;j++){
-                System.out.print("  ");
+                System.out.print(" ");
             }
-            System.out.print("*   ");
-            for(int j=heart-i;j>1;j--){
-                System.out.print("    ");
+            for(int j = 1; j<=side;j++){
+                System.out.print("*");
             }
-            if(i!=heart)
-                System.out.print("*    ");
             System.out.println();
         }
     }
